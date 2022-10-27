2897dce8904379fe3ef9333f049f6de59ef8ff9d
Yitro <yitro.verstraeten@student.howest.be>
2022-10-27 09:58:59 +0200

    added entities for hoezen

---
+++ API/TheCollectiveAPI/TheCollectiveAPI/TableEntities/HoesEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCollectiveAPI.TableEntities
+{
+    internal class HoesEntity : TableEntity
+    {
+        public HoesEntity() { }
+
+
+        public string HoesId { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
---
+++ API/TheCollectiveAPI/TheCollectiveAPI/TableEntities/ListHoezen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCollectiveAPI.TableEntities
+{
+    internal class ListHoezen
+    {
+        public List<HoesEntity> listHoezen { get; set; }
+    }
+}
