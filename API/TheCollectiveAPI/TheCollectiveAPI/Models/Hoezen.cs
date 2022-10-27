856b26a4fe3a006717e45bc7bca1038a1b50bd8a
Yitro <yitro.verstraeten@student.howest.be>
2022-10-27 09:45:40 +0200

    added model hoezen

---
+++ API/TheCollectiveAPI/TheCollectiveAPI/Models/Hoezen.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCollectiveAPI.Models
+{
+    internal class Hoezen
+    {
+        [JsonProperty("hoes_id")]
+        public string HoesId { get; set; }
+
+        [JsonProperty("company_name")]
+        public string CompanyName { get; set; }
+    }
+}
